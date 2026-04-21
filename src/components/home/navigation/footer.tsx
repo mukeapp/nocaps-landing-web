f251da3796edaaf66b5a3b3e180034f978bc8098
Franck Mukendi <franck.mukendi@gmail.com>
2026-04-21 12:33:55 -0400

    Update footer.tsx

--- src/components/home/navigation/footer.tsx
+++ src/components/home/navigation/footer.tsx
@@ -116,7 +116,7 @@ const Footer = () => {
 
             <div className="mt-8 border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between w-full">
                 <p className="text-sm text-muted-foreground mt-8 md:mt-0">
-                    &copy; {new Date().getFullYear()} NoCap. All rights reserved.
+                    &copy; {new Date().getFullYear()} MUKEAPPS LLC. All rights reserved.
                 </p>
             </div>
 
