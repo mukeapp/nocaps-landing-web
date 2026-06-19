4ff8114395660971c40f2d5f2ab4a822a827a6b2
Franck Mukendi <franck.mukendi@gmail.com>
2026-06-19 15:53:49 -0400

    Update nav-links.tsx

--- src/components/home/navigation/nav-links.tsx
+++ src/components/home/navigation/nav-links.tsx
@@ -10,17 +10,20 @@ const NavLinks = () => {
 
   return (
     <ul className="flex items-center justify-center gap-8">
-      <a href={href('pricing')} className="hover:text-foreground/80 text-sm cursor-pointer">
-        Pricing
-      </a>
-      <a href={href('about')} className="hover:text-foreground/80 text-sm cursor-pointer">
-        About
+      <a href={href('what-is-nocap')} className="hover:text-foreground/80 text-sm cursor-pointer">
+        What is NoCaps?
       </a>
       <a href="/features" className="hover:text-foreground/80 text-sm cursor-pointer">
         Features
       </a>
+      <a href={href('about')} className="hover:text-foreground/80 text-sm cursor-pointer">
+        About
+      </a>
       <a href={href('feedback')} className="hover:text-foreground/80 text-sm cursor-pointer">
         Feedback
+      </a>
+      <a href={href('pricing')} className="hover:text-foreground/80 text-sm cursor-pointer">
+        Pricing
       </a>
       <a href="/release-roadmap" className="hover:text-foreground/80 text-sm cursor-pointer">
         Release Roadmap
