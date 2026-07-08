47c89306aecb173cea430257c4943bf38b98d7f3
Franck Mukendi <franck.mukendi@gmail.com>
2026-07-08 03:25:39 -0400

    LoginPageContent

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const AuthLayout = ({ children }: Props) => {
     return (
-        <div style={{ minHeight: '100vh', backgroundColor: '#0D0D0D' }}>
+        <div className="min-h-screen bg-background">
             {children}
         </div>
     )
